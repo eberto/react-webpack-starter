b3ffe54511b384e258d9e4bc6fdd86299ad2168b
Eberto Cepero Abreu <ecepero@prosupply.ec>
2016-07-08 10:54:59 -0500

    - Making first test to react component. - Added script for building vendors. - Karma broken. Doesn't find tests. - Adding typings for react-addons-test-utils.

--- tests/test1.spec.tsx
+++ tests/test1.spec.tsx
@@ -1,19 +1,10 @@
 /// <reference path="../lib/typings/index.d.ts"/>
 
 import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as TestUtils from "react-addons-test-utils";
 
-import * as TestUtils from "reactaddons";
-
-declare var __dirname: any;
-
-console.log("Directorio actual: " + __dirname);
-
-/*
-
-
-import TestUtils = require("./node_modules/react/lib/ReactTestUtils");  //TODO: see why it doesn't resolve module.
 import { Greeter } from "./../src/components/greeter";
-
 
 describe("Greeter", () => {
 
@@ -23,17 +14,8 @@ describe("Greeter", () => {
         component = TestUtils.renderIntoDocument(<Greeter compiler="Webpack" framework="Typescript" />);
     });
 
-    it('true is true', () => {
-        
-        expect(component.getDOMNode().textContent).toMatch(/Hello from Webpack and Typescript!/);
-    });
-});
-
-*/
-
-describe("Lolo", () => {
-
-    it("Lala", () => {
-        console.log("bloh");
+    it('Outputs correct text', () => {
+        var componentDOM = ReactDOM.findDOMNode(component);
+        expect(componentDOM.textContent).toMatch(/Hello from Webpack and Typescript!/);
     });
 });
