b937cd089a2abb13a99e52bb82bd9eaa2d936150
ebertobucket <eberto.cepero@gmail.com>
2016-08-18 12:05:04 -0500

    Playing with table component

--- src/components/clients/index.tsx
+++ src/components/clients/index.tsx
@@ -20,98 +20,158 @@ export interface IClientsProps {
 }
 
 export interface IClientsState {
+	totalWidth: number;
+}
+
+interface IColData {
+    width: number | string;
+    minVisibleContainerWidth?: number;
+    headerText?: string;
+    modelPropName?: string;
 }
 
 export class Clients extends React.Component<IClientsProps, IClientsState> {
+    private colsData: Array<IColData> = [{
+        width: 40
+    }, {
+        headerText: "Nombre",
+        modelPropName: "firstName",
+        width: "25%"
+    }, {
+        headerText: "Apellido",
+        modelPropName: "lastName",
+        width: "25%"
+    }, {
+        headerText: "RUC / CI",
+        modelPropName: "identification",
+        width: 90
+    }, {
+        headerText: "F. Nacimiento",
+        width: 85,
+        minVisibleContainerWidth: 900
+    }, {
+        headerText: "Dirección",
+        modelPropName: "address",
+        width: "25%"
+    }, {
+        headerText: "Teléfono 1",
+        modelPropName: "phone1",
+        width: 70,
+        minVisibleContainerWidth: 900
+    }, {
+        headerText: "Teléfono 2",
+        modelPropName: "phone2",
+        width: 70,
+        minVisibleContainerWidth: 900
+    }, {
+        headerText: "Tel. Móvil",
+        modelPropName: "mobilePhone",
+        width: 80
+    }, {
+        headerText: "Correo E.",
+        modelPropName: "email",
+        width: "25%"
+    }, {
+        headerText: "Edad",
+        modelPropName: "age",
+        width: 40
+    }, {
+        width: 80
+    }];
+
     constructor() {
         super();
-        this.state = { dimensions: { width: 0 }, extraWidth: 0};
+        this.state = { totalWidth: 0 };
     }
-    colStyle(width: number): any {
+    isColVisible(colData: IColData) {
+        var minVisibleWidth = colData.minVisibleContainerWidth;
+        return !minVisibleWidth || this.state.totalWidth > minVisibleWidth;
+    }
+    sumFixed(): number {
+        var result = 0;
+        for(var i = 0; i < this.colsData.length; i++) {
+            var width = this.colsData[i].width;
+            if(typeof width === "number" && this.isColVisible(this.colsData[i])) {
+                result = result + width;
+            }
+        }
+        return result;
+    }
+    calcExtraPercent(): number {
+        var remainingWidth = 0;
+        var totalVisibleCols = 0;
+        for(var i = 0; i < this.colsData.length; i++) {
+            var width = this.colsData[i].width;
+            if(typeof width === "string") {
+                if(!this.isColVisible(this.colsData[i])) {
+                    remainingWidth += parseInt(width.substring(0, width.length - 1));
+                }
+                else {
+                    totalVisibleCols++;
+                }
+            }
+        }
+        console.log(remainingWidth);
+        return remainingWidth / totalVisibleCols;
+    }
+    calcColPercentWidth(width: string): string {
+        var extraWidth = this.state.totalWidth - this.sumFixed();
+        var extraPercent = parseInt(width.substring(0, width.length - 1)) / 100 * extraWidth + this.calcExtraPercent();
+        return (extraPercent * 100 / this.state.totalWidth) + "%";
+    }
+    colStyle(width: number|string): any {
+        var realWidth = width;
+        if(typeof width === "string") {
+            realWidth = this.calcColPercentWidth(width);
+        }
         return {
             paddingLeft: 5,
             paddingRight: 5,
             whiteSpace: "nowrap",
             overflow: "hidden",
             textOverflow: "ellipsis",
-            width: width || Math.max(width || 0, 0)
+            width: realWidth
         };
     }
-    
-    // handleResize() {
-    //     //console.log("resizing...");
-    //     this.setState({});
-    // }
-
-    // componentDidMount() {
-    //     window.addEventListener('resize', this.handleResize.bind(this));
-    // }
-
-    // componentWillUnmount() {
-    //     window.removeEventListener('resize', this.handleResize.bind(this));
-    // }
-
-    private fixedWidthSum = 40 + 85 + 85 + 70 + 70 + 80 + 40 + 80;
-
-    private renderCount = 0;
 
     render() {
-        //console.log("rendering...")
+        console.log("rendering...");
         var tableStyle = {
-            //minWidth: 252,
             tableLayout: "fixed"
         };
-        console.log("rendering times: " + (++this.renderCount));
+        
         return (
             <Paper zDepth={1} style={this.props.style}>
-                <Measure whitelist={["width"]}>
-                    {(dimensions: any) => {
-                        //var fixedWidthSum = 40 + 85 + 85 + 70 + 70 + 80 + 40 + 80;
-                        var extraWidth = Math.max(dimensions.width - this.fixedWidthSum, 0);
-                        //console.log(extraWidth);
-                        //console.log(dimensions.width);
-                        var quarter = extraWidth / 4;
-                        var colWidths =    [40, quarter,  quarter, 85, 85, quarter, 70, 70, 80, quarter, 40, 80];
-                        return <table className="row" style={tableStyle}>
-                            <thead>
-                                <tr className="row">
-                                    <th style={this.colStyle(colWidths[0])}><Checkbox /></th>
-                                    <th style={this.colStyle(colWidths[1])}>Nombre</th>
-                                    <th style={this.colStyle(colWidths[2])}>Apellido</th>
-                                    <th style={this.colStyle(colWidths[3])}>RUC / CI</th>
-                                    <th style={this.colStyle(colWidths[4])}>F. Nacimiento</th>
-                                    <th style={this.colStyle(colWidths[5])}>Dirección</th>
-                                    <th style={this.colStyle(colWidths[6])}>Teléfono 1</th>
-                                    <th style={this.colStyle(colWidths[7])}>Teléfono 2</th>
-                                    <th style={this.colStyle(colWidths[8])}>Tel. Móvil</th>
-                                    <th style={this.colStyle(colWidths[9])}>Correo E.</th>
-                                    <th style={this.colStyle(colWidths[10])}>Edad</th>
-                                    <th style={this.colStyle(colWidths[11])}></th>
+                <Measure whitelist={["width"]} onMeasure={(dimensions: any) => this.setState({totalWidth: dimensions.width})}>
+                    <table style={tableStyle}>
+                        <thead>
+                            <tr>
+                                {this.colsData.map((colData: IColData, index: number) => 
+                                    this.isColVisible(colData)?
+                                        (index === 0?
+                                            <th key={"th_"+index} style={this.colStyle(colData.width)}><Checkbox /></th> :
+                                            <th key={"th_"+index} style={this.colStyle(colData.width)}>{colData.headerText || ""}</th>) :
+                                        false
+                                )}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.props.clients.map((client: Client) => 
+                                <tr key={client.id}>
+                                    {this.colsData.map((colData: IColData, index: number) =>
+                                        this.isColVisible(colData)?
+                                            (index === 0?
+                                                <td key={"td_"+client.id + "_"+index} style={this.colStyle(colData.width)}><Checkbox /></td> :
+                                                <td key={"td_"+client.id + "_"+index} style={this.colStyle(colData.width)}>{(client as any)[colData.modelPropName] || ""}</td>) :
+                                            false
+                                    )}
                                 </tr>
-                            </thead>
-                            <tbody>
-                                {this.props.clients.map((client: Client) =>
-                                    <tr key={client.id}>
-                                        <td style={this.colStyle(colWidths[0])}><Checkbox /></td>
-                                        <td style={this.colStyle(colWidths[1])}>{client.firstName}</td>
-                                        <td style={this.colStyle(colWidths[2])}>{client.lastName}</td>
-                                        <td style={this.colStyle(colWidths[3])}>{client.identification}</td>
-                                        <td style={this.colStyle(colWidths[4])}>1988/01/19</td>
-                                        <td style={this.colStyle(colWidths[5])}>{client.address}</td>
-                                        <td style={this.colStyle(colWidths[6])}>{client.phone1}</td>
-                                        <td style={this.colStyle(colWidths[7])}>{client.phone2}</td>
-                                        <td style={this.colStyle(colWidths[8])}>{client.mobilePhone}</td>
-                                        <td style={this.colStyle(colWidths[9])}>{client.email}</td>
-                                        <td style={this.colStyle(colWidths[10])}>{client.age}</td>
-                                        <td style={this.colStyle(colWidths[11])}>Actions</td>
-                                    </tr>
-                                )}                            
-                                <tr className="row last-row">
-                                    <td className="col s12">1</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    }}
+                            )}
+                            <tr className="last-row">
+                                <td>Last Row for Pagination</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </Measure>
             </Paper>
         );        
